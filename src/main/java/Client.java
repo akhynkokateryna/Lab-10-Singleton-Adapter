2f8a00f8e679ac4b3b4fe4c6daeb32ceaec2c8c6
Kateryna Akhynko <kateryna.akhynko@ucu.edu.ua>
2022-12-01 10:53:02 +0200

    next changes

---
+++ src/main/java/Client.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public interface Client {
+    String getEmail();
+    String getCountry();
+    LocalDate getLastActiveTime();
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        User user = new User(1, "Yaroslav", "ya.prytula", 18);
+        user.save();
+    }
+}
